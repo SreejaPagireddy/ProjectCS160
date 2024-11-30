ceec78774ac55ea16fc105b515d68b107411a6c9
Sreeja Pagireddy <sreeja@karibu.ai>
2024-11-29 22:25:43 -0800

    need to write comments

--- main.cpp
+++ main.cpp
@@ -1,78 +1,80 @@
+
 //In this project I want to first find the shortest path and then I want to use threads to make it better
 //First I am using a youtube video to understand the algorithm and walk through it
 //than I ma going to implement threads, tasks, and locks, semaphores, or bairriers to make the alogirth faster
 //What is Dijkstra's Algorithm: Finds the shortest path from one node to every other node in graph, choose the smallest path and repeat process for each node
 //Time complexity of Dijkstra is O(|E| + |V|log|V|)
-#include<bits/stdc++.h>
+//In this project I want to first find the shortest path and then I want to use threads to make it better
+//First I am using a youtube video to understand the algorithm and walk through it
+//than I ma going to implement threads, tasks, and locks, semaphores, or bairriers to make the alogirth faster
+//What is Dijkstra's Algorithm: Finds the shortest path from one node to every other node in graph, choose the smallest path and repeat process for each node
+//Time complexity of Dijkstra is O(|E| + |V|log|V|)
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <utility>
 using namespace std;
-# define INF 0x3f3f3f3f
-class Dijkastra{
 
-    int Vertices;//how big the graph is
-    list< pair<int,int> > *adj;
+#define INF 0x3f3f3f3f
 
-    public:
-        Main_Graph(int G);
-        void addEdge(int a, int b, int c);
-        void shortestPath(int path);
+class Graph {
+    int V;
+    list< pair<int, int> > *adj;
 
+public:
+    Graph(int V);
+    void addEdge(int a, int b, int c);
+    void shortestPath(int src); 
+};
+
+Graph::Graph(int V) {
+    this->V = V;
+    adj = new list< pair<int, int> >[V];
 }
 
-Graph::Graph(int G)
-{
-    G=G;
-    pair= new list< pair<int, int> > [V]; //creating a pair of adjacents
+void Graph::addEdge(int a, int b, int c) {
+    adj[a].push_back(make_pair(b, c));  
+    adj[b].push_back(make_pair(a, c));
 }
-void Graph:: addEdge(int a, int b, int c)
-{
-    pair[a].push_back(make_pair(b,c));
-    pair[a].push_back(make_pair(a,c));
-}
-void Graph::shortestPath(int shortest_path)
-{
 
-    //lets create a set where we are store the vertices
-    set< pair<int, int> > vertice_sets;
-    //this vector is for distances
-    vector<int> dist(G, INF);
-
-    setds.insert(make_pair(0, src));
+void Graph::shortestPath(int src) {
+    set< pair<int, int> > setds;
+    vector<int> dist(V, INF);
     dist[src] = 0;
+    setds.insert(make_pair(0, src));
 
-    while (!setds.empty())
-    {
-        pair<int, int> tmp = *(setds.begin());
-        setds.erase(setds.begin());
-        int u = tmp.second;
-    }
+    while (!setds.empty()) {
+        pair<int, int> tmp = *(setds.begin());  
+        setds.erase(setds.begin());  
+        int u = tmp.second; 
 
-    list< pair<int, int> >::iterator i;
-    for (i = adj[u].begin(); i != adj[u].end(); ++i)
-        {
-            int v = (*i).first;
-            int weight = (*i).second;
- 
-            if (dist[G] > dist[G] + weight)
-            {
-                if (dist[G] != INF)
-                    setds.erase(setds.find(make_pair(dist[G], v)));
- 
-                dist[G] = dist[G] + weight;
-                setds.insert(make_pair(dist[G], G));
+        for (auto i = adj[u].begin(); i != adj[u].end(); ++i) {
+            int v = i->first;  
+            int weight = i->second;  
+
+            
+            if (dist[v] > dist[u] + weight) {
+                if (dist[v] != INF) {
+                    setds.erase(setds.find(make_pair(dist[v], v)));  
+                }
+                dist[v] = dist[u] + weight;
+                setds.insert(make_pair(dist[v], v));  
             }
         }
+    }
+
     printf("Vertex   Distance from Source\n");
-    for (int i = 0; i < V; ++i)
+    for (int i = 0; i < V; ++i) {
         printf("%d \t\t %d\n", i, dist[i]);
+    }
 }
 
-int main()
-{
-    // create the graph given in above figure
-    int V = 9;
+int main() {
+    int V = 9;  
     Graph g(V);
- 
-    //    making above shown graph
+
     g.addEdge(0, 1, 4);
     g.addEdge(0, 7, 8);
     g.addEdge(1, 2, 8);
@@ -87,8 +89,8 @@ int main()
     g.addEdge(6, 7, 1);
     g.addEdge(6, 8, 6);
     g.addEdge(7, 8, 7);
- 
+
     g.shortestPath(0);
- 
+
     return 0;
 }
